b09607de18ae4dd7d9520e2847ce82c95ca5ed71
jjangjun <adp0615@gmail.com>
2025-02-10 08:58:19 +0900

    MainSection 컴포넌트 리팩토링

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,32 +1,33 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import poster from "../assets/poster.jpeg";
 
 export default function MainSection() {
   const imgRef = useRef(null);
   const [opacity, setOpacity] = useState(1);
 
-  const handleIntersection = useCallback(([entry]) => {
-    setOpacity(entry.intersectionRatio); // 뷰포트에 보이는 비율을 opacity로 설정
-  }, []);
-
   useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null, // 뷰포트 기준
-      threshold: Array.from({ length: 11 }, (_, i) => i * 0.1), // 0.0 ~ 1.0까지 감지
-    });
+    const observer = new IntersectionObserver(
+      ([entry]) => setOpacity(entry.intersectionRatio),
+      {
+        root: null, // 뷰포트 기준
+        threshold: Array.from({ length: 101 }, (_, i) => i * 0.01), // 0.01 ~ 1.00까지 감지
+      }
+    );
 
     if (imgRef.current) observer.observe(imgRef.current);
 
-    return () => observer.disconnect();
-  }, [handleIntersection]);
+    return () => {
+      if (imgRef.current) observer.unobserve(imgRef.current);
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
+    <div className="flex h-screen justify-center items-center">
       <img
         ref={imgRef}
         src={poster}
-        alt="poster"
-        className="h-[30rem] object-contain transition-opacity duration-200 ease-out"
+        alt="overtime poster"
         style={{ opacity }}
       />
     </div>
